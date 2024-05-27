87b1777565ff1338e7e206ab08e2c072deb55285
vinnu2251 <82252677+vinnu2251@users.noreply.github.com>
2024-05-27 05:58:43 +0000

    some review changes on conatiner file

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker docker pull vinay7944/simple-python-flask-app:latest
+ docker pull vinay7944/simple-python-flask-app:latest
 
 # Run the Docker image as a container
  docker run -d -p 5000:5000 vinay7944/simple-python-flask-app:latest
