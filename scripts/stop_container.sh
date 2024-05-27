f9be6f979cda8665070762176c5414f5f889d9bc
vinnu2251 <82252677+vinnu2251@users.noreply.github.com>
2024-05-27 06:47:01 +0000

    second changes on stop container

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-set -e
 
-# Stop the running container (if any)
-docker rm -f
+# Get the IDs of all containers (running and stopped)
+ALL_CONTAINERS=$(docker ps -aq)
+
+if [ -n "$ALL_CONTAINERS" ]; then
+    # Stop all running containers
+    docker stop $ALL_CONTAINERS
+    # Remove all containers
+    docker rm $ALL_CONTAINERS
+    echo "All containers (running and stopped) have been stopped and removed."
+else
+    echo "No containers found."
+fi
